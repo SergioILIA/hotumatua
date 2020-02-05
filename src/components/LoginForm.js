d2aa72835225eab9bdbb4275e722b54caa91a962
SergioILIA <sergio.avendano.ilia@gmail.com>
2020-02-05 17:38:01 -0300

    home

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import logoHotumatua from '../images/logo-hotu-matua.png';
 
 class LoginForm extends React.Component{
 
     render(){
         return(
-            <div>
-                <h1>Bienvenido</h1>
-                <h3>Ingrese su usuario y su clave</h3>
-                <form>
-                    <div className="form-group">
-                        <label>Usuario: </label>
-                        <input className="form-control" type="text" />
+            <React.Fragment>
+                <div className="row d-flex justify-content-center">
+                    <div className="panel col-md-6">
+                        <div className="row d-flex justify-content-center logo-login ">
+                            <img
+                            src={logoHotumatua}
+                            alt="Logo Hotumatua"
+                            />
+                        </div>
+                        <div>
+                            <h1 className="text-center">Bienvenido</h1>
+                            <h3 className="text-center">Ingrese su usuario y su clave</h3>
+                            <form>
+                                <div className="form-group">
+                                    <label className="text-justify font-weight-bold">Usuario </label>
+                                    <input className="form-control" type="text" />
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </form>
-            </div>
+                </div>            
+
+            </React.Fragment>
         );
     }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import LoginForm from '../components/LoginForm';
+import Helmet from 'react-helmet';
+import './styles/Home.css';
 export default class Home extends Component {
 render() 
     { 
         return (
             <React.Fragment>
+                <Helmet>
+                    <style>{'body { background:antiquewhite;}'}</style>
+                </Helmet>
                 <LoginForm />
             </React.Fragment>
         ); 
