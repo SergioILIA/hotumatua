aa6c5a724b9d030f43f8915cd07587087d5dba5c
sergioILIA <sergio.avendano.ilia@gmail.com>
2020-02-05 21:33:03 -0300

    bootstrap changes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,27 +6,38 @@ class LoginForm extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <div className="row d-flex justify-content-center">
-                    <div className="panel col-md-6">
-                        <div className="row d-flex justify-content-center logo-login ">
-                            <img
-                            src={logoHotumatua}
-                            alt="Logo Hotumatua"
-                            />
-                        </div>
-                        <div>
+                <div className="container">
+                    <div className="row d-flex justify-content-center">
+                        <div className="panel col-md-6">
+                            <div className="row d-flex justify-content-center logo-login ">
+                                <img
+                                className="img-fluid"
+                                src={logoHotumatua}
+                                alt="Logo Hotumatua"
+                                />
+                            </div>
                             <h1 className="text-center">Bienvenido</h1>
-                            <h3 className="text-center">Ingrese suu usuario y su clave</h3>
+                            <h3 className="text-center">Ingrese su usuario y su clave</h3>
                             <form>
                                 <div className="form-group">
-                                    <label className="text-justify font-weight-bold">Usuario </label>
-                                    <input className="form-control" type="text" />
+                                    <label for="usuario" className="text-justify font-weight-bold">Usuario </label>
+                                    <input id="usuario" className="form-control" type="text" />
                                 </div>
-                            </form>
+                                <div className="form-group">
+                                    <label for="pass" className="text-justify font-weight-bold">Clave </label>
+                                    <input id="pass" className="form-control" type="password" />
+                                </div>
+                                <button  type="submit" className="btn btn-primary col">ENTRAR</button>
+                            </form>  
                         </div>
-                    </div>
-                </div>            
-
+                    </div>  
+                    <div className="row">    
+                        <a href="" className="col text-center">
+                            <span className="glyphicon glyphicon-wrench" aria-hidden="true"></span>
+                            Olvido su clave
+                        </a>           
+                    </div>            
+                </div>
             </React.Fragment>
         );
     }
